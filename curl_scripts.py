07009c3a64fd34b43fb982d838e9ebf31296c444
blyo <blyo@benjis-MacBook-Pro.local>
2020-05-26 16:37:04 -0500

    20200526 01

---
+++ curl_scripts.py
@@ -0,0 +1,4 @@
+#Office Light 1
+curl --location --request PUT 'http://192.168.0.14/api/bpdiHvLoYj7sGYMjUhjvPMAIOnv9M7tSCtbzmBRp/lights/8/state' \
+--header 'Content-Type: text/plain' \
+--data-raw '{"on":true, "bri":25}'
